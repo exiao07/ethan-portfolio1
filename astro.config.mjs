210368b8182c689f9f202b099a982b725a2dd287
Ethan Xiao <95255806+exiao07@users.noreply.github.com>
2026-01-15 14:53:10 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://exiao07.github.io",
+  base: "/ethan-portfolio1",
   vite: {
     plugins: [tailwindcss()],
   },
